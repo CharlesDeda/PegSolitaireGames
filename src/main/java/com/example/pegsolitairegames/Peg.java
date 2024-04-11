7872113802ab35e7b5ee36d45aabc154c96bfa1a
SeanFulcher <166151469+SeanFulcher@users.noreply.github.com>
2024-04-11 15:35:04 -0400

    addition of the beginning steps
    theres a descrip of a way we could make it work but i want yalls opinoins on if its the best

--- src/main/java/com/example/pegsolitairegames/Peg.java
+++ src/main/java/com/example/pegsolitairegames/Peg.java
@@ -40,4 +40,59 @@ public class Peg {
         circle.setFill(this.getColor());
     }
 
+    public static boolean validPeg(currPeg, endPeg, pegArr){
+        currLoco = currPeg.getNum();
+        //open being boolean var to see it its there
+        for (int i = 0; i > pegArr.length(); i ++){ // idk if we need this loop. Probably same functionality without
+            // example: (currPeg == 0 & (endPeg == 3 & pegArr[3] != None ) || (endPeg == 5 & pegArr[5] != None))
+            if(pegArr[i] == 0 & endPeg == 3 || endPeg == 5) { // if taking off loop, change pegArr[i] to currPeg
+                return true;                                  // also
+            }
+            elif (pegArr[i] == 1 & endPeg == 6 || endPeg == 8){
+                return true;
+            }
+            elif (pegArr[i] == 2 & endPeg == 9 || endPeg == 7){
+                return true;
+            }
+            elif (pegArr[i] == 3 & endPeg == 0 || endPeg == 5 || endPeg == 10 || endPeg == 12){
+                return true;
+            }
+            elif (pegArr[i] == 4 & endPeg == 11 || endPeg == 13){
+                return true;
+            }
+            elif (pegArr[i] == 5 & endPeg == 0 || endPeg == 3 || endPeg == 12 || endPeg == 14){
+                return true;
+            }
+            elif (pegArr[i] == 6 & endPeg == 8 || endPeg == 1){
+                return true;
+            }
+            elif (pegArr[i] == 7 & endPeg == 2 || endPeg == 9){
+                return true;
+            }
+            elif (pegArr[i] == 8 & endPeg == 1 || endPeg == 6){
+                return true;
+            }
+            elif (pegArr[i] == 9 & endPeg == 2 || endPeg == 7){
+                return true;
+            }
+            elif (pegArr[i] == 10 & endPeg == 3 || endPeg ==  12){
+                return true;
+            }
+            elif (pegArr[i] == 11 & endPeg == 4 || endPeg == 13){
+                return true;
+            }
+            elif (pegArr[i] == 12 & endPeg == 3 || endPeg == 5 || endPeg == 10 || endPeg == 14){
+                return true;
+            }
+            elif (pegArr[i] == 13 & endPeg == 4 || endPeg == 11) {
+                return true;
+            }
+            elif (pegArr[i] == 14 & endPeg == 5 || endPeg == 12){
+                return true;
+            }
+
+            return false;
+        }
+
+    }
 }
